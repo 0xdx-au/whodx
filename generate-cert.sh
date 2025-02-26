20c148f6d85d4ee9f0b462a548ae7d7b95a977f4
0xdx-au <130902630+0xdx-au@users.noreply.github.com>
2025-02-27 02:33:30 +1100

    Update generate-cert.sh

--- generate-cert.sh
+++ generate-cert.sh
@@ -8,6 +8,6 @@ if [ ! -f selfsigned.crt ] || [ ! -f selfsigned.key ]; then
     openssl req -x509 -nodes -newkey rsa:2048 \
       -keyout selfsigned.key \
       -out selfsigned.crt \
-      -days 1 \
-      -subj "/C=US/ST=Denial/L=Nowhere/O=whodx/CN=localhost"
+      -days 31 \
+      -subj "/C=US/ST=whodx/L=newphone/O=whodx/CN=localhost"
 fi
